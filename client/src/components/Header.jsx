257f06c962093339ac534373b550157043c37834
pranav-kadam <pranavkadam1111@gmail.com>
2023-07-26 20:29:00 +0530

    Added bg to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 const Header = () => {
+  const headerStyle = {
+    backgroundImage: `url(https://b.zmtcdn.com/web_assets/81f3ff974d82520780078ba1cfbd453a1583259680.png)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "200px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const foodScoutStyle = {
+    color: "white",
+    fontFamily: "Roboto, sans-serif",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    fontSize: "3rem",
+    textAlign: "center",
+  };
+
   return (
-    <div>
-      <h1 className="font-weight-light display-1 text-center">
-        Restaurant Finder
-      </h1>
+    <div style={headerStyle}>
+      <h1 style={foodScoutStyle}>FoodScout</h1>
     </div>
   );
 };
